27d9706589c54d6755aa69c500bd52fb3033f199
Rafflesia Khan <Rafflesia.Khan@ibm.com>
2022-08-02 13:19:09 +0100

    updated with components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,42 @@
-import React from 'react';
-import { Button, TextInput} from "@carbon/react";
-import { View } from "@carbon/react/icons"
+import { useState, ChangeEvent} from "react";
+import { Button, TextInput } from "@carbon/react";
 
-import styles from './App.module.scss';
+import styles from "./App.module.scss";
 
 function App() {
+  const [input, setInput] = useState("");  
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>)  => {
+    setInput(event.target.value);
+  };
+
+  const onButtonClick = () => {
+    alert("Hi " + input +", Have a nice day!!");
+  };
+
   return (
     <div className={styles.appDiv}>
-       <h1>Welcome to Get started with React, TypeScript, and Carbon 11 </h1>
+      <h1>Welcome to Get started with React, TypeScript, and Carbon 11 </h1>
+      <div className={styles.bodyDiv}>
+        <TextInput
+          id="text-input-1"
+          type="text"
+          labelText="Text input label"
+          helperText="Optional help text"
+          value={input}
+          onChange={handleChange}
+        />
+
+        <Button
+          kind="primary"
+          size="sm"
+          type="submit"
+          iconDescription={"Description"}
+          tooltipAlignment="end"
+          tooltipPosition="left"
+          onClick={onButtonClick}
+        >Click !</Button>
+      </div>
     </div>
   );
 }
