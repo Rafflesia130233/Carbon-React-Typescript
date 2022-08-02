70a73c28cfd9448a0ecf8a174b7ee229bf609280
Rafflesia Khan <Rafflesia.Khan@ibm.com>
2022-08-02 12:17:09 +0100

    app style added

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { Button, TextInput} from "@carbon/react";
 import { View } from "@carbon/react/icons"
 
+import styles from './App.module.scss';
+
 function App() {
   return (
-    <div>
+    <div className={styles.appDiv}>
        <h1>Welcome to Get started with React, TypeScript, and Carbon 11 </h1>
     </div>
   );
